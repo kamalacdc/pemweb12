b2e4724b072b23993c600a85b8c600721413a66c
kamalacdc <ahmaddiankamal@saintek.unipdu.ac.id>
2024-11-04 22:55:12 +0530

    pertemuan6

---
+++ pertemuan 6/11.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Contoh list dengan PHP</title>
+</head>
+
+<body>
+    <h2>Daftar Absensi Mahasiswa</h2>
+    <ol>
+        <?php
+        for ($i = 1; $i <= 10000; $i++) {
+          echo "<li>Nama Mahasiswa ke-$i</li>";
+        }
+    ?>
+    </ol>
+</body>
+
+</html>
---
+++ pertemuan 6/tugas 6.php
@@ -0,0 +1,27 @@
+<?php
+
+$pemasukan = 50000000;
+
+// Hutang A
+$hutangA = 16500000;
+$bungaHutangA = 0.05 * $hutangA; // Bunga 5%
+
+// Hutang B
+$hutangB = 9500000;
+$bungaHutangB = 0.045 * $hutangB; // Bunga 4.5%
+
+
+// Total Bunga Hutang
+$totalBungaHutang = $bungaHutangA + $bungaHutangB;
+
+// Total Hutang
+$totalHutang = $hutangA + $hutangB + $totalBungaHutang;
+
+// Sisa Uang
+$sisaUang = $pemasukan - $totalHutang;
+
+
+echo "Sisa Uang: Rp" . number_format($sisaUang, 0, ',', '.') . "<br>";
+echo "Jumlah Total Bunga Hutang: Rp" . number_format($totalBungaHutang, 0, ',', '.') . "<br>";
+echo "Jumlah Total Hutang: Rp" . number_format($totalHutang, 0, ',', '.') . "<br>";
+?>
